1b4148120c1ba2818ec1e7b1e02237a1636387c2
hazee03 <haozhe2003@gmail.com>
2025-07-10 15:05:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,3 +17,30 @@ try:
   st.dataframe(df)
 except Exception as e:
   st.error(f"Failed to load data: {e}")
+
+
+
+">> https://github.com/settings/personal-access-tokens"
+a = st.text_input("Enter a")
+b = st.text_input("Enter b")
+c = st.text_input("Enter c")
+
+if st.button("Upload to GitHub"):
+  df = pd.DataFrame([{"a": a, "b": b, "c": c}])
+  csv = df.to_csv(indec=False)
+  content = base64.b64encode(csv.encode()).decode()
+
+url = "https://api.github.com/repos/hazee03/testing/contents/data2.csv"
+headers = {"Authorization": f"token {st.secrets['github']['token']}"}
+
+payload = {
+  "message": "Add data.csv",
+  "content": content,
+  "branch": "main"
+}
+
+r = requests.put(url, headers=headers, json=payload)
+if r.status code in [200,201]:
+    st.success("data uploaded!")
+else:
+    st.error("Failed tp upload")
